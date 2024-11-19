c535a22ef8f7c637dbcf3b6516798911e39b3f34
Alexander Smirnov <sanyablask@gmail.com>
2024-11-19 22:05:47 +0300

    Add files via upload

---
+++ 'module_1_4.py'.py
@@ -0,0 +1,8 @@
+#my_string = input("Интересно ходить босиком: ")
+print( "Здорово, я делаю все супер" .upper())
+print("Салам, у меня снизу живут соседи" .upper().lower())
+print('у меня все будет хорошо'.replace( ' ', '' ))
+first_name = 'Интересно ходить босиком'
+print(first_name[0])
+first_name = 'Интересно ходить босиком'
+print(first_name[-1])
